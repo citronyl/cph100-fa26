6fe24ca768541142368b193a8bec71a630379648
citronyl <git@jhl04.com>
2026-09-09 01:19:04 -0700

    Implement .fit and .transform functions and finish get_numerical_vectorizer function

--- psets/pset1/vectorizer.py
+++ psets/pset1/vectorizer.py
@@ -18,6 +18,7 @@ class Vectorizer:
         :return: function to map numerical x to a zero mean, unit std dev normalized score.
         """
 
+        values = [float(v) for v in values]
         mean, std = np.mean(values), np.std(values)
 
         if std == 0:
@@ -33,7 +34,7 @@ class Vectorizer:
             Hint: this fn knows mean and std from the outer scope
             """
 
-            return (x-mean)/std
+            return np.array([(float(x) - mean)/std])
             #NotImplementedError("Not implemented")
 
         return vectorizer
@@ -51,14 +52,34 @@ class Vectorizer:
             """
             bin_idx = np.digitize
 
-        #raise NotImplementedError("Histogram vectorizer not implemented yet")
+        raise NotImplementedError("Histogram vectorizer not implemented yet")
 
     def get_categorical_vectorizer(self, values):
         """
         :return: function to map categorical x to one-hot feature vector
         """
+
+        # values are not guaranteed to be numerical, so use indexes instead
+
+        unique_cats = sorted(list(set(str(v) for v in values)))
+        cat_idx = {cat: idx for idx, cat in enumerate(unique_cats)}
+        num_cats = len(unique_cats)
+
+        def vectorizer(x):
+            one_hot = np.zeros(num_cats, dtype=int)
+            val = str(x)
+            if val in cat_idx: 
+                 one_hot[cat_idx[val]] = 1
+            return one_hot
+
+        return vectorizer
+        
         raise NotImplementedError("Categorical vectorizer not implemented yet")
 
+    def get_ordinal_vectorizer(self, values):
+
+         raise NotImplementedError("Ordinal vectorizer not implemented yet")
+    
     def fit(self, X):
         """
             Leverage X to initialize all the feature vectorizers (e.g. compute means, std, etc)
@@ -67,8 +88,23 @@ class Vectorizer:
             This implementation will depend on how you design your feature config.
         """
 
-        raise NotImplementedError("Not implemented yet")
-        self.feature_transforms = { "transform_name": None}
+        # raise NotImplementedError("Not implemented yet")
+        for feature in self.feature_config.get("numerical", []):
+            values = [row[feature] for row in X if feature in row]
+            self.feature_transforms[feature] = self.get_numerical_vectorizer(values)
+
+        for feature in self.feature_config.get("categorical", []):
+                values = [row[feature] for row in X if feature in row]
+                self.feature_transforms[feature] = self.get_categorical_vectorizer(values)
+
+        for feature in self.feature_config.get("histogram", []):
+                values = [row[feature] for row in X if feature in row]
+                self.feature_transforms[feature] = self.get_histogram_vectorizer(values)
+
+        for feature in self.feature_config.get("ordinal", []):
+                values = [row[feature] for row in X if feature in row]
+                self.feature_transforms[feature] = self.get_ordinal_vectorizer(values)
+        #self.feature_transforms = { "transform_name": None}
         self.is_fit = True
 
 
@@ -83,6 +119,18 @@ class Vectorizer:
             raise Exception("Vectorizer not intialized! You must first call fit with a training set" )
 
         transformed_data = []
-        raise NotImplementedError("Not implemented yet")
+
+        for row in X:
+            row_features = []
+
+            for feature, transform_fn in self.feature_transforms.items():
+                raw = row.get(feature, None)
+                transformed_val = transform_fn(raw)
+                row_features.append(transformed_val)
+
+            concat_row = np.concatenate(row_features)
+            transformed_data.append(concat_row)
+                
+        #raise NotImplementedError("Not implemented yet")
 
         return np.array(transformed_data)
